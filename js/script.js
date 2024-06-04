0c41b5469065e3680cce708011ef2e1201a87a26
magda <mkroman97@gmail.com>
2024-06-04 10:23:17 +1000

    Completed challenge

--- js/script.js
+++ js/script.js
@@ -30,10 +30,32 @@ const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
 
-//console.log(sora, clover, baxter, cleo, francine);
+clover.isTired = 8;
+francine.isTired = 9;
 
-//clover.sleep();
-//baxter.play();
+const allPets = [
+    sora,
+    clover,
+    baxter,
+    cleo,
+    francine
+];
 
-//console.log(clover, baxter);
+//console.log(allPets);
 
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}.`;
+        pets.append(li);
+    }
+};
+
+statusButton.addEventListener("click", function () {
+    showPets(allPets);
+});
